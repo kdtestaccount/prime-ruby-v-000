6ed72b76fe83fe953463f7d74309a68b7786b05d
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-08 18:35:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(int_arg)
-  return false if integer < 2
-  (2..integer - 1).each do |x|
-    if (integer % x) == 0
+  return false if int_arg < 2
+  (2..int_arg - 1).each do |x|
+    if (int_arg % x) == 0
       return false
     end
   end
